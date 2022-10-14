0428fd3405586519255a7d6df4fad7c7957dacd8
danielqueirosene <42477879+danielqueirosene@users.noreply.github.com>
2022-10-14 18:43:43 -0300

    Add files via upload

---
+++ Control.java
@@ -0,0 +1,75 @@
+
+package Control;
+public class UsersManager () {
+
+ try {
+              FileOutUsersManager fos = new
+              FileOutUsersManager("NetDevices.txt");
+              ObjectOutUsersManager oos = new ObjectOutUsersManager(fos);
+              oos.writeObject(mNetDevices);
+              oos.close();
+              fos.close();
+
+    private string login;
+    private string senha; 
+    public UserManager (string login, string senha){
+        this.login = login;
+        this.senha = senha;
+
+        void saveNetDevices() {
+           
+            } catch (UsersManager nexc) {
+              JOptionPane.showMessageDialog(null, "NetDevices list is null - No devices saved", "Saving NetDevices", JOptionPane.INFORMATION_MESSAGE);
+            } catch (UsersManager nexc) {
+              JOptionPane.showMessageDialog(null, "NetDevices list is invalid - No devices saved", "Saving NetDevices", JOptionPane.INFORMATION_MESSAGE);
+            } catch (UsersManager nexc) {
+              JOptionPane.showMessageDialog(null, "NetDevices list is not serializable - No devices saved", "Saving NetDevices", JOptionPane.INFORMATION_MESSAGE);
+            } catch (UsersManager nexc) {
+              JOptionPane.showMessageDialog(null, "IO Exception saving NetDevices list - No devices saved", "Saving NetDevices", JOptionPane.INFORMATION_MESSAGE);
+            }
+          }
+
+}
+public class ValidateUserDetails {
+    private string login;
+    login string = 12;
+    //12 caracteres que não seja vazios e sem numeros
+    try {
+              FileOutValidateUserDetails fos = new
+              FileOutValidateUserDetails("NetDevices.txt");
+              ObjectOutValidateUserDetails oos = new ObjectOutUsersManager(fos);
+              oos.writeObject(mNetDevices);
+              oos.login();
+              fos.close();
+    public static void main(String[] args)   
+	{ 
+		String login = "abcd";           
+		Boolean flag=true;
+		for(int login=0;a<input.length();a++)
+		{
+			if(login != 0 && input.charAt(a) == '-')
+                flag=false;
+                if(login != null && input.charAt(a) == '-')
+                flag=false;
+			if( !Character.isDigit(input.charAt(a)))
+				flag=false;          	   
+		}
+		if(flag)
+		{
+			System.out.println(input+" is valid Integer");
+		}
+
+        void ValidateUserDetails() {
+        
+            } catch (ValidateUserDetails nexc) {
+              JOptionPane.showMessageDialog(null, "NetDevices list is null - No devices saved", "Saving NetDevices", JOptionPane.INFORMATION_MESSAGE);
+            } catch (ValidateUserDetails nexc) {
+              JOptionPane.showMessageDialog(null, "NetDevices list is invalid - No devices saved", "Saving NetDevices", JOptionPane.INFORMATION_MESSAGE);
+            } catch (ValidateUserDetails nexc) {
+              JOptionPane.showMessageDialog(null, "NetDevices list is not serializable - No devices saved", "Saving NetDevices", JOptionPane.INFORMATION_MESSAGE);
+            } catch (ValidateUserDetails nexc) {
+              JOptionPane.showMessageDialog(null, "IO Exception saving NetDevices list - No devices saved", "Saving NetDevices", JOptionPane.INFORMATION_MESSAGE);
+            }
+          }
+	} 
+}
