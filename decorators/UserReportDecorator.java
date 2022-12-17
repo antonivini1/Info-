c5ef417030f9145968c0b1991c5082acc5d66bf4
Gabriel Camboim <gabrielcamboim99@gmail.com>
2022-12-16 23:03:47 -0300

    feat: Atualizado UserReportDecorator com os atributos dinamicos

--- decorators/UserReportDecorator.java
+++ decorators/UserReportDecorator.java
@@ -8,4 +8,34 @@ public class UserReportDecorator extends UserReport {
     public UserReportDecorator(UserReport umUserReport) {
        userReport = umUserReport;
     }
+
+    @Override
+     public String getUsername() {
+        return firstName + " " + username + " " + lastName;
+    }
+
+    @Override
+    public int getVisits() {
+        return visits + selfVisit;
+    }
+
+    @Override
+    public int getMinutesLoggedIn() {
+        return minutesLoggedIn + minutesLoggedInBuffer;
+    }
+
+    @Override
+    public int getComments() {
+        return comments + selfComments;
+    }
+
+    @Override
+    public int getTickets() {
+        return tickets + selfTickets;
+    }
+
+    @Override
+    public int getItems() {
+        return items + selfItems;
+    }
 }
